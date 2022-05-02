fb5a787482f81798e95b3907ceff2268008b6263
Mansi Thakkar <Mansi.Thakkar@einfochips.com>
2022-05-02 14:44:13 +0530

    Python file

---
+++ MaxNumber.py
@@ -0,0 +1,8 @@
+def maximum():
+    a=int(input("Enter A: "))
+    b=int(input("Enter B: "))
+    if(a>b):
+        print("Maximum no(A): ",a)
+    else:
+        print("Maximum no(B): ",b)
+maximum()
