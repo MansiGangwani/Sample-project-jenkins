e610192ece254d4daca5ae69487666995fdc41d5
MansiGangwani <87477104+MansiGangwani@users.noreply.github.com>
2022-06-15 14:21:10 +0530

    Update MaxNumber.py

--- MaxNumber.py
+++ MaxNumber.py
@@ -7,10 +7,10 @@ def maximum():
     """
     print("Maximum of two numbers")
     print("======================")
-    # no1 = 6
-    # no2 = 4
-    no1 = int(input("Enter No1: "))
-    no2 = int(input("Enter No2: "))
+    no1 = 6
+    no2 = 4
+#     no1 = int(input("Enter No1: "))
+#     no2 = int(input("Enter No2: "))
     print("no1: ", no1, "   ", "no2:", no2)
 
     if no1 > no2:
