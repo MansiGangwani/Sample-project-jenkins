8dfec8b106de6c3423fa2d4caeb3f8463aeeb042
MansiGangwani <87477104+MansiGangwani@users.noreply.github.com>
2022-06-16 14:14:19 +0530

    Update and rename MaxNumber-user.py to MaxNumberUser.py

--- MaxNumberUser.py
+++ MaxNumberUser.py
@@ -14,9 +14,9 @@ def maximum():
     print("no1: ", no1, "   ", "no2:", no2)
 
     if no1 > no2:
-        print("Mno1ximum no(no1): ", no1)
+        print("Maximum no(no1): ", no1)
     else:
-        print("Mno1ximum no(no2): ", no2)
+        print("Maximum no(no2): ", no2)
 
 
 maximum()
